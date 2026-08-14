60637ae9101c7fccecaf763412190c762ad9e14f
Cameron Smith <csmotoeng@gmail.com>
2026-08-14 21:46:55 +0100

    Complete Pit Crew project foundation

--- Source/PitCrew/PitCrew.Build.cs
+++ Source/PitCrew/PitCrew.Build.cs
@@ -14,9 +14,6 @@ public class PitCrew : ModuleRules
 			"Engine",
 			"InputCore",
 			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
 			"UMG",
 			"Slate"
 		});
